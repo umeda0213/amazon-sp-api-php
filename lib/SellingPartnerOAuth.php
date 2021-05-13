f5734de13c0ca11eee7de34dda2edf699b92b437
umeda0213 <umeda.sr.ibri@gmail.com>
2021-05-13 21:32:19 +0900

    Update SellingPartnerOAuth.php

--- lib/SellingPartnerOAuth.php
+++ lib/SellingPartnerOAuth.php
@@ -103,7 +103,7 @@ class SellingPartnerOAuth
         $body = $response->getBody()->getContents();
         $bodyAsJson = json_decode($body, true);
         if (isset($bodyAsJson['error_description'])) {
-            throw new SellingPartnerOAuthException($bodyAsJson['error_description'], $bodyAsJson['error']);
+            throw new SellingPartnerOAuthException($bodyAsJson['error_description'] . ' / ' . $bodyAsJson['error']);
         }
 
         return $bodyAsJson['refresh_token'];
