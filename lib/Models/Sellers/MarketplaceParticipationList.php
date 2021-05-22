c7987bca580eeea0cc426f34cd4220e9f057222e
umeda0213 <umeda.sr.ibri@gmail.com>
2021-05-22 16:10:38 +0900

    fix

--- lib/Models/Sellers/MarketplaceParticipationList.php
+++ lib/Models/Sellers/MarketplaceParticipationList.php
@@ -167,7 +167,7 @@ class MarketplaceParticipationList implements ModelInterface, ArrayAccess, Itera
      */
     public function listInvalidProperties()
     {
-        $invalidProperties = parent::listInvalidProperties();
+        $invalidProperties = [];// parent::listInvalidProperties();
 
         return $invalidProperties;
     }
